168291154eb59d7393095f0aff98b6b8433ce93b
mineogo <192755374+MINEOGO@users.noreply.github.com>
2026-06-28 02:36:51 +0530

    oops

--- setup_powershell.ps1
+++ setup_powershell.ps1
@@ -1,9 +1,15 @@
 Set-Location $PSScriptRoot
+$installed = $false
 if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
     winget install --id Git.Git -e --source winget --accept-package-agreements --accept-source-agreements
+    $installed = $true
 }
 if (-not (Get-Command node -ErrorAction SilentlyContinue)) {
     winget install OpenJS.NodeJS --accept-package-agreements --accept-source-agreements
+    $installed = $true
+}
+if ($installed) {
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 }
 git pull rynzemini main *>$null
 if (-not (Test-Path $PROFILE)) {
